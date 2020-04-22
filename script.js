0e29bfa1bb39661b92ef8f2b32e0fbb1d0a5921b
Yuliia Borys <yuliannaboris@gmail.com>
2020-04-22 11:09:02 +0200

    oop program

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
 'use strict';
 
-DomElement
+document.querySelector('body').style.cssText = 'background-color:#ebe3dd;textAlign:center;';
+//let selector = ('#selecto');
+//let selector = ('.select');
+
+function DomElement(height, width, fontSize, selector, backgroundcolor) {
+    this.height = Number(height);
+    this.width = width;
+    this.fontSize = fontSize;
+    this.selector = selector;
+    this.backgroundcolor = backgroundcolor;
+    
+}
+DomElement.prototype.selecKtor = function() {
+    if (this.selector == '.select') {
+    const neWdiv = document.createElement('div');
+    neWdiv.classList.add('.select');
+    neWdiv.textContent = 'Сегодня хороший день!';
+    document.body.append( neWdiv);
+    }
+    if(this.selector == '#selecto') {
+    const neWp = document.createElement('p'); 
+    neWp.classList.add('#select');
+    document.body.append( neWp);
+    neWp.textContent = 'До того, как начнёте изучать способы работы со стилями и классамив JavaScript,есть одно важное правило. Надеемся, это достаточно очевидно, но мы все равно должны об этом упомянуть.';
+
+    }     
+};
+
+const doMElem = new DomElement('100px', '100%', '18px', '#selecto', '#f0dbdb');
+console.log(doMElem);
+doMElem.selecKtor();
+
+
+
+
+
+
+
+
+
+
+    
+
+
